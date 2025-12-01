7d937bde8ee925c2ebb4d81b06a5f5591f897068
AnumasaGayathri <gayathrianumasa124@gmail.com>
2025-12-01 18:38:17 +0530

    TASK15.4
    Updated API key and added assertion for Delhi.

--- ASSIGNMENT-15/TASK-15.4.py
+++ ASSIGNMENT-15/TASK-15.4.py
@@ -1,6 +1,6 @@
 import requests # type: ignore
 import json
-API_KEY = "e6c6083509fc4d450cde0ca4414b3a9f"
+API_KEY = 44772518fccd2cd9bac7667117e5bf29
 def get_weather_with_errors(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     try:
@@ -33,4 +33,5 @@ def fetch_weather(city):
     return result
 assert fetch_weather("New York")["city"] == "New York"
 assert fetch_weather("xyz123") is None
+
 assert isinstance(fetch_weather("Delhi"), dict)
